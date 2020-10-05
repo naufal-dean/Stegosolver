a4317a4828f40ad71ca65b0a61557d75cb55e2d4
naufal-dean <naufal.dean@gmail.com>
2020-10-05 17:17:51 +0700

    Remove comment

--- src/main.py
+++ src/main.py
@@ -5,24 +5,6 @@ from PyQt5.QtCore import QFile
 from ui.gui import MainWindow
 from controller import StegoAudio, StegoImage, StegoImageBPCS, StegoVideo
 
-# if __name__ == '__main__':
-#     ic = StegoImage('example/raw.png')
-#     ic.insert_data('main.py', False, 'someseed')
-#     ic.image.save('test.png')
-#     oc = StegoImage('test.png')
-#     f = oc.extract_data('test.txt', 'someseed')
-#     print(f)
-#     exit()
-# if __name__ == '__main__':
-#     key = "STEGANO"
-#     path = "example/test.wav"
-#     hide = StegoAudio(key, path)
-#     hide.insert_data("example/test.pdf", 1)
-#     hide.save_stego_audio("example/save.wav")
-#     extractor = StegoAudio(key, "example/save.wav")
-#     extractor.extract_data()
-#     extractor.save_extracted_file("example/cok.pdf")
-#     psnr("example/test.wav", "example/save.wav")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
