d23b404e1cdb914585a72ba3c7a74685921bdc5f
naufal-dean <naufal.dean@gmail.com>
2020-10-05 10:34:02 +0700

    Clean navigation menu

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -13,31 +13,42 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi(os.getcwd() + '/ui/main.ui', self)
+        self.setup_ui()
 
+    def setup_ui(self):
         self.stackedWidget.setCurrentIndex(0)
+        # main menu
         self.imageButton.clicked.connect(self.image)
         self.audioButton.clicked.connect(self.audio)
+        # image menu
+        self.imageHideNavBtn.clicked.connect(self.imageHide)
+        self.imageExtractNavBtn.clicked.connect(self.imageExtract)
+        self.imageBackMainMenuNavBtn.clicked.connect(self.main_menu)
+        # image hide
+        self.backImagePageNavBtn.clicked.connect(self.image)
+        # image extract
+        self.backImagePageNavBtnE.clicked.connect(self.image)
+        # audio menu
+        self.AHidePageButton.clicked.connect(self.audioHide)
+        self.AHideButton.clicked.connect(self.audioHideFile)
+
+    def main_menu(self):
+        self.stackedWidget.setCurrentIndex(0)
 
     def image(self):
         self.stackedWidget.setCurrentIndex(1)
-        self.imageHideNavBtn.clicked.connect(self.imageHide)
-        self.imageExtractNavBtn.clicked.connect(self.imageExtract)
 
     def imageHide(self):
         self.stackedWidget.setCurrentIndex(2)
-        self.backImagePageNavBtn.clicked.connect(self.image)
 
     def imageExtract(self):
         self.stackedWidget.setCurrentIndex(3)
-        self.backImagePageNavBtnE.clicked.connect(self.image)
 
     def audio(self):
         self.stackedWidget.setCurrentIndex(4)
-        self.AHidePageButton.clicked.connect(self.audioHide)
 
     def audioHide(self):
         self.stackedWidget.setCurrentIndex(5)
-        self.AHideButton.clicked.connect(self.audioHideFile)
 
     def audioHideFile(self):
         audio_path = self.audioTextEdit.toPlainText()
